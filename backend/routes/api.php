a2ab5bae801aa979dbea6071475b62cc272b3ac1
xcode17 <do.crazy192@gmail.com>
2022-08-29 01:29:14 +0700

    add: route jwt api

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -2,18 +2,30 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\{RegisterController,LoginController,LogoutController};
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+/**
+ * route "/user"
+ * @method "GET"
+ */
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * route "/register"
+ * @method "POST"
+ */
+Route::post('/register', RegisterController::class)->name('register');
+
+/**
+ * route "/login"
+ * @method "POST"
+ */
+Route::post('/login', LoginController::class)->name('login');
+
+/**
+ * route "/logout"
+ * @method "POST"
+ */
+Route::post('/logout', LogoutController::class)->name('logout');
